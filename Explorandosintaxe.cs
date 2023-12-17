513424687a99446407d16de7d820e707c335688e
Matheus O Silva <oliveiramatheus709@gmail.com>
2023-12-17 15:04:29 -0300

    master

---
+++ Explorandosintaxe.cs
@@ -0,0 +1,32 @@
+using System;
+
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+
+
+    string email = Console.ReadLine();
+
+    string nome = Console.ReadLine();
+
+    string senha = Console.ReadLine();
+
+
+
+    Console.WriteLine($"{nome}, verifique o email: {email} para ativar.");
+
+
+
+
+
+    }
+
+}
+
+   
